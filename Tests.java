a70a7c123a832f56831903c9e32a3583c4e1c800
Konstantin46 <99416843+Konstantin46@users.noreply.github.com>
2022-02-22 22:42:12 +0300

    Add files via upload

---
+++ Tests.java
@@ -0,0 +1,11 @@
+package lesson_3;
+
+public class Tests {
+    public static void main(String[] args) {
+
+    }
+
+    static void newObjectTest() {
+        MusicPlayer player = new MusicPlayer();
+    }
+}
